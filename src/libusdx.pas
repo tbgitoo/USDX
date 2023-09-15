582d04573c782b94f16c4216ba301313e1c1c94f
tbgitoo <thomas.braschler@gmail.com>
2023-09-15 23:11:17 +0200

    copy over .so files via script

--- src/libusdx.pas
+++ src/libusdx.pas
@@ -1,5 +1,24 @@
 library USDX;
 
+{$I switches.inc}
+
+uses
+  //heaptrc,
+  {$IFDEF Unix}
+  cthreads,            // THIS MUST be the first used unit in FPC if Threads are used!!
+                       // (see http://wiki.lazarus.freepascal.org/Multithreaded_Application_Tutorial)
+  cwstring,            // Enable Unicode support
+  {$ENDIF}
+
+  {$IFNDEF FPC}
+  ctypes                 in 'lib\ctypes\ctypes.pas', // FPC compatibility types for C libs
+  {$ENDIF}
+
+    //------------------------------
+  //Includes - 3rd Party Libraries
+  //------------------------------
+  SQLiteTable3  in 'lib\SQLite\SQLiteTable3.pas',
+  SQLite3       in 'lib\SQLite\SQLite3.pas';
 
 
 function func4C():Real;	 cdecl;
@@ -7,8 +26,16 @@ begin
    func4C:=18.0;
 end;
 
+
+
+
 exports
   func4C name 'func4C';
+
+
+
+
+
 
 begin
 
